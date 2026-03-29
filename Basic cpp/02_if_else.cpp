9926f44b91c11ded9bb81a84e6b7a90a87f795be
Sadaf Sadeqeen Chowdhury <s.sadeqeen1212@gmail.com>
2026-03-30 02:17:51 +0600

    Add if-else example in 02_if_else.cpp

---
+++ Basic cpp/02_if_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    // Check condition
+    if (a > 10)
+    {
+        cout << "Greater than 10" << endl;
+    }
+    else
+    {
+        cout << "Less or equal to 10" << endl;
+    }
+    return 0;
+}
