26aed962fcae0ea8340accc2ecfff13dd02916c5
Sadaf Sadeqeen Chowdhury <s.sadeqeen1212@gmail.com>
2026-04-24 10:20:05 +0600

    Add basic map usage example in C++
    Implement basic map operations in C++.

---
+++ ALL STL function/map/01_map_basic.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, int> mp;
+
+    mp["Sakib al hasan"] = 79;
+    mp.insert({"akib", 100});
+    mp.insert({"akib", 150}); // overwrite না, insert ignore হবে (key duplicate হলে)
+    mp["tamim"] = 79;
+
+    for(auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+
+    cout << mp.count("akib") << endl;
+
+    return 0;
+}
